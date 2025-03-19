8a751e7cb92f2f4aa9341e7670505c361b0e8f08
terencengdev <terencejiahao@gmail.com>
2025-03-19 12:13:37 +0800

    updated link

--- src/app/[locale]/component/AppStoreBtn.tsx
+++ src/app/[locale]/component/AppStoreBtn.tsx
@@ -3,7 +3,10 @@ export default function AppStoreBtn() {
   return (
     <div className="py-10 max-w-[300px] md:max-w-[320px] mx-auto app-store-wrap grid grid-cols-2 gap-4">
       <div>
-        <a href="#" target="_blank">
+        <a
+          href="https://play.google.com/store/apps/details?id=com.BlackwellGlobalInvestmentsUKLimited.pelican&pli=1"
+          target="_blank"
+        >
           <Image
             src="/google-play.jpg"
             alt="Google Play"
@@ -14,7 +17,10 @@ export default function AppStoreBtn() {
         </a>
       </div>
       <div>
-        <a href="#" target="_blank">
+        <a
+          href="https://apps.apple.com/gb/app/blackwell-invest/id1666036351"
+          target="_blank"
+        >
           <Image
             src="/app-store.jpg"
             alt="App store"
